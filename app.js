0040030c1f36e0e29cea37f09831848e0aff777b
DipRRai <superderped@gmail.com>
2024-11-12 17:58:57 +1100

    commit

--- app.js
+++ app.js
@@ -3,10 +3,26 @@ const path = require('path')
 const app =  express();
 const PORT = 3000;
 const server = app.listen(PORT, () => console.log('server started'));
+
+const io = require('socket.io')(server)
+
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+let socketsConnected = new Set()
 
+io.on('connection', (socket) => {
+    console.log(socket.id)
+    socketsConnected.add(socket.id)
 
+    io.emit('clients-total', socketsConnected.size)
+
+    socket.on('disconnect', ()=>{
+        socketsConnected.delete(socket.id)
+        io.emit('clients-total', socketsConnected.size)
+    })
+
+    socket.on('message', (data) => {
+        console.log(data);
+        socket.broadcast.emit('chat-message', data)
+    })
+})
